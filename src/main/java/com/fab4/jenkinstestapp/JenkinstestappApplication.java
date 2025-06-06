1274be5cb785585f9d9e0815c7b8c414db393525
pillaiyedhu <pillaiyedhu@gmail.com>
2025-06-06 18:10:34 +0530

    Initial commit - basic springboot app

---
+++ src/main/java/com/fab4/jenkinstestapp/JenkinstestappApplication.java
@@ -0,0 +1,13 @@
+package com.fab4.jenkinstestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinstestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinstestappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fab4/jenkinstestapp/JenkinstestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fab4.jenkinstestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinstestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
